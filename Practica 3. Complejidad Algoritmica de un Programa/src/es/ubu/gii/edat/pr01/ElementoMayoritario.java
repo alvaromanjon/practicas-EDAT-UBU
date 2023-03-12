4ef833c5357fc0c69b88a1941ea9485cd097047e
Álvaro Manjón <alvaro.manjon@me.com>
2023-03-12 17:42:10 +0100

    Add mayoritarioIterativo()

--- Practica 3. Complejidad Algoritmica de un Programa/src/es/ubu/gii/edat/pr01/ElementoMayoritario.java
+++ Practica 3. Complejidad Algoritmica de un Programa/src/es/ubu/gii/edat/pr01/ElementoMayoritario.java
@@ -4,6 +4,7 @@ package es.ubu.gii.edat.pr01;
  * Clase que alberga el código completo para la Práctica 01
  * 
  * @author bbaruque
+ * @author Álvaro Manjón Vara
  *
  */
 public class ElementoMayoritario {
@@ -41,16 +42,31 @@ public class ElementoMayoritario {
 	}
 
 	/**
-	 * 
-	 * @param <E>
-	 * @param array
-	 * @return
+	 * Aproximación iterativa en la que se van a recorrer todos los elementos de la lista y
+	 * se va a comprobar la frecuencia de cada uno de ellos.
+	 *
+	 * @param array - array de elementos en el que se va a buscar el elemento mayoritario
+	 * @return objeto de tipo RespuestaMayoritaria con el elemento mayoritario y su frecuencia
 	 */
-	public static <E> RespuestaMayoritaria<E> mayoritarioIterativo (E [] array){
+	public static <E> RespuestaMayoritaria<E> mayoritarioIterativo (E [] array) {
+		int frecMax = 0;
+		E elemMax = null;
 
-		// TODO Completar el metodo iterativo
-		return null;
-		
+		for (E elem : array) {
+			for (E elem2 : array) {
+				if (elem.equals(elem2)) {
+					frecMax++;
+				}
+			}
+			if (frecMax > array.length / 2) {
+				elemMax = elem;
+				break;
+			} else {
+				frecMax = 0;
+			}
+		}
+
+		return new RespuestaMayoritaria<E>(elemMax, frecMax);
 	}
 
 	/**
@@ -61,11 +77,9 @@ public class ElementoMayoritario {
 	 */
 	public static <E> RespuestaMayoritaria<E> mayoritarioRecursivo (E[] array){
 		
-		// TODO Completar el metodo recursivo
+
+
 		return null;
 
 	}
-	
-	// Es posible emplear métodos auxiliares para la ayuda a la implementación de los métodos obligatorios.
-	
 }
