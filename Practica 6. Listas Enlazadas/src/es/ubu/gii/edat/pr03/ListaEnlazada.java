b2816c45c27e6c123ecf47da879c82b8542b609d
Álvaro Manjón <alvaro.manjon@me.com>
2023-04-16 20:01:51 +0200

    feat: Add constructor and inner class

--- Practica 6. Listas Enlazadas/src/es/ubu/gii/edat/pr03/ListaEnlazada.java
+++ Practica 6. Listas Enlazadas/src/es/ubu/gii/edat/pr03/ListaEnlazada.java
@@ -3,7 +3,40 @@ package es.ubu.gii.edat.pr03;
 import java.util.AbstractList;
 import java.util.Iterator;
 
-public class ListaEnlazada extends AbstractList<E> {
+public class ListaEnlazada<E> extends AbstractList<E> {
+    private NodoInterno<E> posicion;
+    private int numElementos;
+
+    public ListaEnlazada() {
+        posicion = null;
+        numElementos = 0;
+    }
+
+    private class NodoInterno<E> {
+        private E elemento;
+        private NodoInterno<E> siguiente;
+
+        public NodoInterno(E elemento) {
+            this.elemento = elemento;
+            this.siguiente = null;
+        }
+
+        public E getElemento() {
+            return elemento;
+        }
+
+        public void setElemento(E elemento) {
+            this.elemento = elemento;
+        }
+
+        public NodoInterno<E> getSiguiente() {
+            return siguiente;
+        }
+
+        public void setSiguiente(NodoInterno<E> siguiente) {
+            this.siguiente = siguiente;
+        }
+    }
 
     @Override
     public boolean add(E e) {
