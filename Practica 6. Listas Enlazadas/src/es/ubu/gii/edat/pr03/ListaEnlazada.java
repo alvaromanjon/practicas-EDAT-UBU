315dc24e9857aa88a106e795b6c9779f590de548
Álvaro Manjón <alvaro.manjon@me.com>
2023-04-16 23:21:23 +0200

    docs: Add javadoc documentation

--- Practica 6. Listas Enlazadas/src/es/ubu/gii/edat/pr03/ListaEnlazada.java
+++ Practica 6. Listas Enlazadas/src/es/ubu/gii/edat/pr03/ListaEnlazada.java
@@ -1,44 +1,92 @@
 package es.ubu.gii.edat.pr03;
 
 import java.util.AbstractList;
-import java.util.Iterator;
 import java.util.ListIterator;
 
+/**
+ * Implementación de una lista enlazada simple, cuyos elementos disponen de un puntero que apunta al
+ * siguiente elemento. La lista funciona en un sólo sentido.
+ *
+ * @author Álvaro Manjón Vara
+ *
+ * @param <E>
+ */
 public class ListaEnlazada<E> extends AbstractList<E> {
     private NodoInterno<E> inicial;
     private int numElementos;
 
+    /**
+     * Constructor de la clase ListaEnlazada. Inicializa la lista vacía.
+     */
     public ListaEnlazada() {
         inicial = null;
         numElementos = 0;
     }
 
+    /**
+     * Clase interna que representa un nodo de la lista enlazada. Cada nodo contiene un elemento y un
+     * puntero al siguiente nodo.
+     *
+     * @param <E>
+     */
     private static class NodoInterno<E> {
         private E elemento;
         private NodoInterno<E> siguiente;
 
+        /**
+         * Constructor de la clase NodoInterno. Inicializa el nodo con el elemento indicado y el
+         * puntero al siguiente nodo a null.
+         *
+         * @param elemento - elemento que se va a almacenar en el nodo
+         */
         public NodoInterno(E elemento) {
             this.elemento = elemento;
             this.siguiente = null;
         }
 
+        /**
+         * Método que devuelve el elemento almacenado en el nodo.
+         *
+         * @return elemento almacenado en el nodo
+         */
         public E getElemento() {
             return elemento;
         }
 
+        /**
+         * Método que modifica el elemento almacenado en el nodo.
+         *
+         * @param elemento - nuevo elemento que se va a almacenar en el nodo
+         */
         public void setElemento(E elemento) {
             this.elemento = elemento;
         }
 
+        /**
+         * Método que devuelve el puntero al siguiente nodo.
+         *
+         * @return puntero al siguiente nodo
+         */
         public NodoInterno<E> getSiguiente() {
             return siguiente;
         }
 
+        /**
+         * Método que modifica el puntero al siguiente nodo.
+         *
+         * @param siguiente - nuevo puntero al siguiente nodo
+         */
         public void setSiguiente(NodoInterno<E> siguiente) {
             this.siguiente = siguiente;
         }
     }
 
+    /**
+     * Método que añade un elemento al final de la lista.
+     *
+     * @param e - elemento que se va a añadir
+     * @return true
+     */
     @Override
     public boolean add(E e) {
         NodoInterno<E> nuevoNodo = new NodoInterno<E>(e);
@@ -55,6 +103,12 @@ public class ListaEnlazada<E> extends AbstractList<E> {
         return true;
     }
 
+    /**
+     * Método que añade un elemento en la posición indicada.
+     *
+     * @param index - posición en la que se va a añadir el elemento
+     * @param element - elemento que se va a añadir
+     */
     @Override
     public void add(int index, E element) {
         if (index < 0 || index >= numElementos) {
@@ -75,6 +129,12 @@ public class ListaEnlazada<E> extends AbstractList<E> {
         this.numElementos++;
     }
 
+    /**
+     * Método que elimina el elemento de la posición indicada.
+     *
+     * @param index - posición del elemento que se va a eliminar
+     * @return contenido del elemento eliminado
+     */
     @Override
     public E remove(int index) {
         E nodoEliminado = null;
@@ -96,6 +156,12 @@ public class ListaEnlazada<E> extends AbstractList<E> {
         return nodoEliminado;
     }
 
+    /**
+     * Método que devuelve el elemento de la posición indicada.
+     *
+     * @param index - posición del elemento que se va a devolver
+     * @return contenido del elemento
+     */
     @Override
     public E get(int index) {
         if (index < 0 || index >= numElementos) {
@@ -109,27 +175,57 @@ public class ListaEnlazada<E> extends AbstractList<E> {
         return nodoPosicion.getElemento();
     }
 
+    /**
+     * Tamaño de la lista.
+     *
+     * @return tamaño de la lista
+     */
     @Override
     public int size() {
         return numElementos;
     }
 
+    /**
+     * Método que vacía la lista
+     */
+    @Override
+    public void clear() {
+        inicial = null;
+        numElementos = 0;
+    }
+
+    /**
+     * Clase iterador interna que implementa la interfaz ListIterator, para poder iterar por la lista.
+     */
     public class IteradorListaEnlazada implements ListIterator<E> {
         private NodoInterno<E> nodoPosicion;
         private NodoInterno<E> nodoAnterior;
         private int indicePosicion;
 
+        /**
+         * Constructor de la clase IteradorListaEnlazada.
+         */
         public IteradorListaEnlazada() {
             nodoPosicion = inicial;
             nodoAnterior = null;
             indicePosicion = 0;
         }
 
+        /**
+         * Método que indica si hay un elemento siguiente.
+         *
+         * @return true si hay un elemento siguiente, false en caso contrario
+         */
         @Override
         public boolean hasNext() {
             return nodoPosicion != null;
         }
 
+        /**
+         * Método que devuelve el elemento siguiente.
+         *
+         * @return elemento siguiente
+         */
         @Override
         public E next() {
             if (!hasNext()) {
@@ -142,11 +238,21 @@ public class ListaEnlazada<E> extends AbstractList<E> {
             return elemento;
         }
 
+        /**
+         * Método que indica si hay un elemento anterior.
+         *
+         * @return true si hay un elemento anterior, false en caso contrario
+         */
         @Override
         public boolean hasPrevious() {
             return nodoAnterior != null;
         }
 
+        /**
+         * Método que devuelve el elemento anterior.
+         *
+         * @return elemento anterior
+         */
         @Override
         public E previous() {
             if (!hasPrevious()) {
@@ -163,11 +269,21 @@ public class ListaEnlazada<E> extends AbstractList<E> {
             return elemento;
         }
 
+        /**
+         * Método que devuelve la posición del elemento siguiente.
+         *
+         * @return posición del elemento siguiente
+         */
         @Override
         public int nextIndex() {
             return indicePosicion;
         }
 
+        /**
+         * Método que devuelve la posición del elemento anterior.
+         *
+         * @return posición del elemento anterior
+         */
         @Override
         public int previousIndex() {
             return indicePosicion - 1;
@@ -189,11 +305,5 @@ public class ListaEnlazada<E> extends AbstractList<E> {
         public void add(E e) {
             throw new UnsupportedOperationException();
         }
-    }
-
-    @Override
-    public void clear() {
-        inicial = null;
-        numElementos = 0;
     }
 }
