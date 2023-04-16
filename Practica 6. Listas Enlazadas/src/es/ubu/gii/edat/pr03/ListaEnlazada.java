1f5f874924c1f0901189054b5a760f1a72a4d1a5
Álvaro Manjón <alvaro.manjon@me.com>
2023-04-16 19:27:31 +0200

    chore: Add initial files

---
+++ Practica 6. Listas Enlazadas/src/es/ubu/gii/edat/pr03/ListaEnlazada.java
@@ -0,0 +1,42 @@
+package es.ubu.gii.edat.pr03;
+
+import java.util.AbstractList;
+import java.util.Iterator;
+
+public class ListaEnlazada extends AbstractList<E> {
+
+    @Override
+    public boolean add(E e) {
+        // TODO: Implementar
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        // TODO: Implementar
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        // TODO: implementar
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO: Implementar
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // TODO: Implementar
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        // TODO: Implementar
+    }
+}
---
+++ Practica 6. Listas Enlazadas/tst/es/ubu/gii/edat/pr03/TestSuitePr06.java
@@ -0,0 +1,13 @@
+package es.ubu.gii.edat.pr03;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+  TestListaEnlazada.class,
+  TestRendimientoListaEnlazada.class
+})
+
+public class TestSuitePr06 {}
