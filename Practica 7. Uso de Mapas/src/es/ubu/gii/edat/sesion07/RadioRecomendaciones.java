4494fc793df4377a04aebaff12045ff15bf1f654
Álvaro Manjón <alvaro.manjon@me.com>
2023-04-23 20:56:12 +0200

    docs: Add JavaDocs

--- Practica 7. Uso de Mapas/src/es/ubu/gii/edat/sesion07/RadioRecomendaciones.java
+++ Practica 7. Uso de Mapas/src/es/ubu/gii/edat/sesion07/RadioRecomendaciones.java
@@ -2,13 +2,36 @@ package es.ubu.gii.edat.sesion07;
 
 import java.util.*;
 
+/**
+ * Clase que contiene una lista de canciones, así como las relaciones entre las distintas canciones. Se
+ * puede generar una playlist a partir de una canción inicial, así como obtener las canciones que no
+ * tienen recomendaciones.
+ *
+ * @author Álvaro Manjón Vara
+ */
 public class RadioRecomendaciones {
     private HashMap<String, ArrayList<String>> listaRecomendaciones;
 
+    /**
+     * Constructor de la clase RadioRecomendaciones. Inicializa la lista de recomendaciones vacía.
+     */
     RadioRecomendaciones() {
         this.listaRecomendaciones = new HashMap<String, ArrayList<String>>();
     }
 
+    /**
+     * Método que inserta las canciones y sus recomendaciones. El array de cadenas de texto contiene las
+     * canciones y sus recomendaciones en el siguiente formato:
+     * String[] temas = { "|", "Tema0", "Tema1","Tema2",
+     * "|", "Tema1", "Tema3","Tema5","Tema12",
+     * "|", "Tema2", "Tema4", "|", "Tema3", "Tema4","Tema2","Tema8","Tema1", "|", "Tema4", "Tema1","Tema2",
+     * "|", "Tema5", "Tema4","Tema2","Tema0","Tema9", "|"}
+     * donde cada separador "|" indica el inicio de un nuevo elemento. El primer elemento es el tema
+     * principal, y los siguientes son las recomendaciones de dicho tema.
+     *
+     * @param recomendaciones - array de cadenas de texto que contiene las canciones y sus recomendaciones
+     * @return número de canciones que se han insertado en la lista de recomendaciones
+     */
     public int insertaRecomendaciones(String[] recomendaciones) {
         int canciones = 0;
         boolean nuevoElemento = false;
@@ -32,6 +55,20 @@ public class RadioRecomendaciones {
         return canciones;
     }
 
+    /**
+     * Método que genera una playlist a partir de una canción inicial y un número de temas.
+     * La playlist se genera de la siguiente forma:
+     * 1. Se añade la canción inicial a la playlist.
+     * 2. Se añaden las recomendaciones de la canción inicial a la playlist.
+     * 3. Si todavía quedan canciones por añadir a la playlist, se añaden las recomendaciones de la
+     * siguiente canción de la lista después de la canción inicial.
+     * 4. Se repite el paso 3 hasta que se hayan añadido todas las canciones a la playlist.
+     * La playlist no va a contener canciones duplicadas.
+     *
+     * @param inicial - canción inicial de la playlist
+     * @param numTemas - número de temas de la playlist
+     * @return playlist generada
+     */
     public String[] generaPlaylist(String inicial, int numTemas) {
         String[] playlist = new String[numTemas];
         ArrayList<String> temasRecomendados = new ArrayList<String>();
@@ -51,6 +88,12 @@ public class RadioRecomendaciones {
         return playlist;
     }
 
+    /**
+     * Método interno que devuelve el número de elementos no nulos de un array de cadenas de texto.
+     *
+     * @param array - array de cadenas de texto
+     * @return número de elementos no nulos del array
+     */
     private int elementosNoNulos(String[] array) {
         int elementos = 0;
         for (int i = 0; i < array.length; i++) {
@@ -61,6 +104,11 @@ public class RadioRecomendaciones {
         return elementos;
     }
 
+    /**
+     * Método que devuelve las canciones que no son recomendación de ninguna otra canción.
+     *
+     * @return canciones que no son recomendación de ninguna otra canción
+     */
     public String[] noRecomendadas() {
         ArrayList<String> temasNoRecomendados = new ArrayList<String>();
         ArrayList<String> todosLosTemas = new ArrayList<String>();
