c22bba162144a8c72895217c0d6ce4c514b4df47
Álvaro Manjón Vara <alvaro.manjon@me.com>
2023-04-24 14:03:48 +0200

    feat: Add initial files

---
+++ Practica 8. Implementacion de Mapas/src/es/ubu/gii/edat/pr04/MapaValoresUnicos.java
@@ -0,0 +1,29 @@
+package es.ubu.gii.edat.pr04;
+
+import java.util.AbstractMap;
+import java.util.Set;
+
+public class MapaValoresUnicos<K,V> extends AbstractMap<K,V> {
+
+    public MapaValoresUnicos() {
+
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        // TODO: Implementar
+        return null;
+    }
+
+    // TODO: Ver si hace falta sobreescribir más métodos
+
+    public MapaValoresUnicos inverso()  {
+        // TODO: Implementar
+        return null;
+    }
+
+    public V forcePut (K key, V value) {
+        // TODO: Implementar
+        return null;
+    }
+}
