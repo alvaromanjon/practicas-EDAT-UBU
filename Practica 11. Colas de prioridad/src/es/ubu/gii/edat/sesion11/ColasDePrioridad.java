2124c2797ca5918d1e78a98c430d60a65f9b9f6e
Álvaro Manjón <alvaro.manjon@me.com>
2023-05-26 20:01:20 +0200

    feat: Add initial files

---
+++ Practica 11. Colas de prioridad/src/es/ubu/gii/edat/sesion11/ColasDePrioridad.java
@@ -0,0 +1,26 @@
+package es.ubu.gii.edat.sesion11;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ColasDePrioridad {
+    public static <E extends Comparable<E>> List<E> mayores(Iterable<E> items, int num) {
+        // TODO implementar
+        return null;
+    }
+
+    public static <E> List<E> mayores(Iterable<E> items, int num, Comparator<E> comparator) {
+        // TODO implementar
+        return null;
+    }
+
+    public static <E extends Comparable<E>> List<E> menores(Iterable<E> items, int num) {
+        // TODO implementar
+        return null;
+    }
+
+    public static <E> List<E> menores(Iterable<E> items, int num, Comparator<E> comparator) {
+        // TODO implementar
+        return null;
+    }
+}
