3bf387b82e8c9663e9cbb70b95c2c4002e122521
Álvaro Manjón <alvaro.manjon@me.com>
2023-05-26 20:58:45 +0200

    feat: First implementation of methods

--- Practica 11. Colas de prioridad/src/es/ubu/gii/edat/sesion11/ColasDePrioridad.java
+++ Practica 11. Colas de prioridad/src/es/ubu/gii/edat/sesion11/ColasDePrioridad.java
@@ -1,26 +1,66 @@
 package es.ubu.gii.edat.sesion11;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ColasDePrioridad {
     public static <E extends Comparable<E>> List<E> mayores(Iterable<E> items, int num) {
-        // TODO implementar
-        return null;
+        PriorityQueue<E> cola = new PriorityQueue<>();
+
+        for (E item : items) {
+            cola.add(item);
+            if (cola.size() > num) {
+                cola.poll();
+            }
+        }
+
+        List<E> resultado = new ArrayList<>(cola);
+        resultado.sort(Collections.reverseOrder());
+        return resultado;
     }
 
     public static <E> List<E> mayores(Iterable<E> items, int num, Comparator<E> comparator) {
-        // TODO implementar
-        return null;
+        PriorityQueue<E> cola = new PriorityQueue<>(comparator);
+
+        for (E item : items) {
+            cola.add(item);
+            if (cola.size() > num) {
+                cola.poll();
+            }
+        }
+
+        List<E> resultado = new ArrayList<>(cola);
+        resultado.sort(Collections.reverseOrder());
+        return resultado;
     }
 
     public static <E extends Comparable<E>> List<E> menores(Iterable<E> items, int num) {
-        // TODO implementar
-        return null;
+        PriorityQueue<E> cola = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (E item : items) {
+            cola.add(item);
+            if (cola.size() > num) {
+                cola.poll();
+            }
+        }
+
+        List<E> resultado = new ArrayList<>(cola);
+        resultado.sort(Comparator.naturalOrder());
+        return resultado;
+
     }
 
     public static <E> List<E> menores(Iterable<E> items, int num, Comparator<E> comparator) {
-        // TODO implementar
-        return null;
+        PriorityQueue<E> cola = new PriorityQueue<>(Collections.reverseOrder(comparator));
+
+        for (E item : items) {
+            cola.add(item);
+            if (cola.size() > num) {
+                cola.poll();
+            }
+        }
+
+        List<E> resultado = new ArrayList<>(cola);
+        resultado.sort(comparator);
+        return resultado;
     }
 }
